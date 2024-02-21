c023259c30c7433daac37a0e54b9d65334f3562e
leetaeyu <ehdrkq2005@naver.com>
2024-02-21 13:01:16 +0900

    로컬에서 유저 정보 받아오기

--- nomadcoder/vanila_javascript/chrome/js/app.js
+++ nomadcoder/vanila_javascript/chrome/js/app.js
@@ -1,1 +1,28 @@
-console.log(412442);
+window.onload = () => {
+    const userName = document.getElementById('userName');
+    const loginForm = document.getElementById('loginForm');
+    const user = document.getElementById('user');
+    const TAG_HIDDEN = 'none';
+    
+    onLoginSubmit = (event) => {
+        event.preventDefault();
+        const userNameValue = userName.value;
+        loginForm.classList.add(TAG_HIDDEN);
+        localStorage.setItem('userName', userNameValue);
+        printGreetings(userNameValue);
+    }
+
+    printGreetings = (userName) => {
+        user.innerText = `환영합니다 ${userName}님`
+        user.classList.remove(TAG_HIDDEN);
+    }
+    
+    const saveUserName = localStorage.getItem('userName');
+    
+    if(saveUserName === null) {
+        loginForm.classList.remove(TAG_HIDDEN);
+        loginForm.addEventListener('submit', onLoginSubmit);
+    } else {
+        printGreetings(saveUserName);
+    }
+}
