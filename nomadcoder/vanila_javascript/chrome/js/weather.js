538f0ea88815b7a870b2d8d1ac8af4acd2e30153
leetaeyu <ehdrkq2005@naver.com>
2024-02-28 12:57:17 +0900

    날씨, 지역 정보 받아오기

--- nomadcoder/vanila_javascript/chrome/js/weather.js
+++ nomadcoder/vanila_javascript/chrome/js/weather.js
@@ -1,4 +1,21 @@
+function onGeoOk(position){
+    const API_KEY = '44ecf81e60f7f3c3c3b905375b92723a';
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const weather = document.querySelector('#weather span:first-child');
+      const city = document.querySelector('#weather span:last-child');
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    })
+  
+}
+function onGeoError(){
+    alert("can't find you. No weather for you.");
+}
 window.addEventListener('load', () => {
-    navigator.geolocation.getCurrentPosition();
-    //#8.0 Geolocation 1minus
+    navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 })
